8d949a6cb8dbd339df49e3fdf78daf8e59220fea
Gayane Grigoryan <44817376+Gayane05@users.noreply.github.com>
2019-09-21 16:35:24 +0400

    Create ReverseInteger.cpp
    Given a 32-bit signed integer, reverse digits of an integer.
    Example 1:
    
    Input: 123
    Output: 321
    Example 2:
    
    Input: -123
    Output: -321
    Example 3:
    
    Input: 120
    Output: 21
    Note:
    Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.

---
+++ ReverseInteger.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int reverse(int x) 
+    {
+        int number = 0;
+        while (x != 0)
+        {
+            if (number > INT_MAX/10 || (number == INT_MAX / 10 && (x % 10) > 7)) 
+            {
+                return 0;
+            }
+            if (number < INT_MIN/10 || (number == INT_MIN / 10 && (x % 10) < -8)) 
+            {
+                return 0;
+            }
+		    number = number * 10 + (x % 10);
+		    x /= 10;
+	    }    
+    return number;
+    }
+};
