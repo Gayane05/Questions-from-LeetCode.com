26158ce06c1e8abce0e650a8e644a3262a161480
Gayane Grigoryan <44817376+Gayane05@users.noreply.github.com>
2020-08-30 11:27:14 +0400

    Update Valid Palindrome.cpp

--- Valid Palindrome.cpp
+++ Valid Palindrome.cpp
@@ -37,58 +37,33 @@ bool isPalindrome(std::string s)
 		mySet.insert(i + 64);
 	}
 
-	//std::string str{ "" };
-	//for (int i = 0; i < s.size(); ++i)
-	//{
-	//	if (mySet.find(s[i]) != mySet.end())
-	//	{
-	//		str.push_back(s[i]);
-	//	}
-	//	else if (mySet.find(s[i] - 32) != mySet.end())
-	//	{
-	//		str.push_back(s[i] - 32);
-	//	}
-	//}
-	//for (int i = 0; i < str.size() / 2; ++i)
-	//{
-	//	if (str[i] != str[str.size() - i - 1])
-	//	{
-	//		return false;
-	//	}
-	//}
-	//return true;
-
-	//std::for_each(s.begin(), s.end(), [](char& c) {
-	//	c = ::toupper(c); });
-		int i = 0;
-		int j = s.size() - 1;
-		for (; i < s.size() / 2; ++i, --j)
+	int i = 0;
+	int j = s.size() - 1;
+	for (; i < s.size() / 2; ++i, --j)
+	{
+		while (!isLetter(s[i], mySet) && i != s.size())
 		{
-			while (!isLetter(s[i], mySet) && i != s.size())
-			{
-				++i;
-			}
-			while (!isLetter(s[j], mySet) && j >= s.size() / 2)
-			{
-				--j;
-			}
-			
-			s[i] = toupper(static_cast<unsigned char>(s[i]));
-			s[j] = toupper(static_cast<unsigned char>(s[j]));
-			if (s[i] != s[j])
-			{
-				return false;
-			}
-			return true;
+			++i;
 		}
-
+		while (!isLetter(s[j], mySet) && j >= s.size() / 2)
+		{
+			--j;
+		}
+		
+		s[i] = toupper(static_cast<unsigned char>(s[i]));
+		s[j] = toupper(static_cast<unsigned char>(s[j]));
+		if (s[i] != s[j])
+		{
+			return false;
+		}
+		return true;
+	}
 }
 
 int main()
 {
 
 	std::string s{ ".," };
-
 	isPalindrome(s);
 
 	return 0;
