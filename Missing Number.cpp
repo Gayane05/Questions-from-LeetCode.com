e23435227d88f8d52dc6faa82facf49bc6231505
Gayane Grigoryan <44817376+Gayane05@users.noreply.github.com>
2020-12-27 21:21:20 +0400

    Missing Number.cpp
    Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
    
    Follow up: Could you implement a solution using only O(1) extra space complexity and O(n) runtime complexity?
    
     
    
    Example 1:
    
    Input: nums = [3,0,1]
    Output: 2
    Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2  is the missing number in the range since it does not appear in nums.

---
+++ Missing Number.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm> 
+
+int missingNumber(std::vector<int>& nums) 
+{
+	// 122 / 122 test cases passed.
+	// Runtime : 32 ms
+	// Memory Usage : 18.3 MB
+
+	int max = 0;
+	int sumInArray = 0;
+
+	for (int i = 0; i < nums.size(); ++i)
+	{
+		if (nums[i] > nums[max])
+		{
+			max = i;
+		}
+		sumInArray += nums[i];
+	}
+
+	int expectedSum = 0;
+	// Gauss' Formula 
+	// ∑ i = 0  i = (n(n + 1)) / 2;
+	//  n
+	expectedSum = (nums.size() * (nums.size() + 1)) / 2;
+
+	// Not Gauss' Formula
+	/*
+	int k = std::max(nums[max], static_cast<int>(nums.size()));
+
+	for (int i = 1; i <= k; ++i)
+	{
+		expectedSum += i;
+	}*/
+
+	return expectedSum - sumInArray;
+}
+
+int main()
+{
+	std::vector<int> nums{ 0, 1 };
+
+	missingNumber(nums);
+
+	return 0;
+}
